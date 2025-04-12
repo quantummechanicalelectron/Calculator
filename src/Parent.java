b066f57aee3ec7ee3a03ade052e40488bd7a5472
Johann Tewes <JohannTewes01001010@gmail.com>
2025-04-12 22:34:58 +0200

    test

--- src/Parent.java
+++ src/Parent.java
@@ -23,4 +23,8 @@ public class Parent {
     public int String2int(String start){
         return Integer.parseInt(start);
     }
+
+    public void test(){
+        System.out.println("fuck yo bitch ass hole");
+    }
 }
